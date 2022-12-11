57c3acadfdaf1f8951781d756fcc9a574161f925
GecikaDM <kaci_gdm@hotmail.com>
2022-12-11 18:35:51 +0100

    active-sections

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-const section = document.querySelectorAll(".section");
-//console.log(section);
+const sections = document.querySelectorAll(".section");
 const navbarItems = document.querySelectorAll(".navbar-items");
-//console.log(navbarItems);
 const navbarItem = document.querySelectorAll(".navbar-item");
-//console.log(navbarItem);
 const mainContent = document.querySelector(".main-content");
 
 
@@ -11,11 +8,31 @@ function pageTransition() {
     for (let i = 0; i  < navbarItem.length; i++) {
     navbarItem[i].addEventListener("click", function() {
       let currentBtn = document.getElementsByClassName("active-btn");
-      console.log(currentBtn)
+      //console.log(currentBtn)
       currentBtn[0].className = currentBtn[0].className.replace(" active-btn", "");
       /* be carrefully, this. doesn't exist with an arrow function */
       this.className += " active-btn";
     });
   }
+  // sections active
+  mainContent.addEventListener('click', (e) => {
+    //console.log(e.target);
+    const id = e.target.dataset.id;
+    //console.log(id);
+    if(id) {
+      //remove selected from the other btn
+      navbarItems.forEach((btn) => {
+        btn.classList.remove('active');
+      })
+      e.target.classList.add('active');
+
+      //hide other sections 
+      sections.forEach((section) => {
+        section.classList.remove('active');
+      })
+      const element = document.getElementById(id);
+      element.classList.add('active');
+    }
+  });
 }
 pageTransition();
